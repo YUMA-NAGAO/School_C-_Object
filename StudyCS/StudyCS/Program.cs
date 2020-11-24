cc4aa1d0497c3bad9f38cb071db12c52c64df2a4
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2020-11-24 21:39:42 +0900

    [add]C#を実行するための環境を整備した

---
+++ StudyCS/StudyCS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stArrayData = Console.ReadLine().Trim().Split(' ');
+
+            int i = int.Parse(stArrayData[0]);
+            int j = int.Parse(stArrayData[1]);
+            Console.WriteLine(i + j);
+
+        }
+    }
+}
